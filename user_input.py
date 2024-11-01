dbf0409df7c8f4b7f1245fffe0d510dfce58d35c
oladeleajayi <oladeleajayi37@gmail.com>
2024-11-01 14:46:30 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,17 @@
+# # user name
+name = input('My name is Mary:')
+print(f'Hello {name}!')
+
+# User age
+age = input(' i am 25 years old')
+print (f' {age} ')
+
+# user location
+location = input( ' and i live in Nigeria')
+print(f' {location}')
+
+# # user details
+name = input('My name is Mary:')
+age =input( 'i am 25 years old')
+location = input(' and i live in Nigeria')
+print(f' {name} {age} {location}' )
